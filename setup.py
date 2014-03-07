4bdcb49d22730b9760966ef6c49eadb3f68ff279
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-03-07 21:08:03 +0100

    Python trove classifiers. Closes #32.
    Technically I think we'd only need Python 2 and 3. I'm leaving the
    parent for clarity (users) and. Also, as suggested by George Hickman,
    I'm adding the versions we are currently actively testing.

--- setup.py
+++ setup.py
@@ -28,5 +28,10 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2'
+        'Programming Language :: Python :: 2.6'
+        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.3'
     ],
 )
