bc52d9f39b0f90ea9656e734119b63c8b47945f0
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-05-16 09:43:23 +0200

    trove classifiers fix

--- setup.py
+++ setup.py
@@ -28,11 +28,11 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2'
-        'Programming Language :: Python :: 2.6'
-        'Programming Language :: Python :: 2.7'
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.3'
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
