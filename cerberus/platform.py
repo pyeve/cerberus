ac491c92601e71daca9fae22a9482040387f6759
Frank Sachsenheim <funkyfuture@riseup.net>
2016-01-28 18:29:51 +0100

    Makes the software ready for Python 4

--- cerberus/platform.py
+++ cerberus/platform.py
@@ -3,9 +3,9 @@
 import sys
 
 
-if sys.version_info[0] == 3:
-    _str_type = str
-    _int_types = (int,)
-else:
+if sys.version_info[0] == 2:
     _str_type = basestring  # noqa
     _int_types = (int, long)  # noqa
+else:
+    _str_type = str
+    _int_types = (int,)
