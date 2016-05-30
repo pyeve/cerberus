8525a31f97aad690eb97613c4f97d0f5224f4ece
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-05-30 14:56:24 +0200

    Add 'Stay Informed' and 'Useful Links' sections to sidebar
    Using python-requests.org as a template.

--- docs/conf.py
+++ docs/conf.py
@@ -154,6 +154,7 @@ html_static_path = ['_static']
 html_sidebars = {
     '**': [
             'about.html',
+            'sidebarintro.html',
             'navigation.html',
             'relations.html',
             'searchbox.html',
