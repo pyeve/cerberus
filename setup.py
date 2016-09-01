f0feb1b3837258bdd7819668b59e2961c0390c2f
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-09-01 10:01:40 +0200

    Bump trove classifier to Production/Stable (5)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[],
     keywords=['validation', 'schema', 'dictionaries'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: ISC License (ISCL)',
