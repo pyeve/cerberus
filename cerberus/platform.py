4f0abd080114df90666469b7efaaf28d65917cd5
Michael Klich <michal@michalklich.com>
2017-01-17 22:17:08 +0100

    Redesign the approach
    Move unicode handling to platform module as it depends on
    the Python version.
    Update tests.

--- cerberus/platform.py
+++ cerberus/platform.py
@@ -1,6 +1,8 @@
 """ Platform-dependent objects """
 
+import copy
 import sys
+from functools import wraps
 
 
 PYTHON_VERSION = float(sys.version_info[0]) + float(sys.version_info[1]) / 10
@@ -12,3 +14,31 @@ if PYTHON_VERSION < 3:
 else:
     _str_type = str
     _int_types = (int,)
+
+
+def py2_error_unicode_fix(f):
+    """Cerberus error messages expect regular binary strings.
+    If unicode is used in schema message can't be printed.
+
+    This decorator ensures that if legacy Python is used unicode
+    strings are encoded before passing to a function.
+    """
+    @wraps(f)
+    def wrapped(obj, error):
+        if PYTHON_VERSION < 3:
+            error = copy.copy(error)
+            error.document_path = _encode(error.document_path)
+            error.schema_path = _encode(error.schema_path)
+            error.constraint = _encode(error.constraint)
+            error.value = _encode(error.value)
+            error.info = _encode(error.info)
+
+        return f(obj, error)
+    return wrapped
+
+
+def _encode(value):
+    """Helper encoding unicode strings into binary utf-8"""
+    if isinstance(value, unicode):
+        return value.encode('utf-8')
+    return value
