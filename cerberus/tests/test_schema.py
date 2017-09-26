528fbfdee01870115ab8f2519e863230dd7bb0ce
Frank Sachsenheim <funkyfuture@riseup.net>
2017-09-26 17:55:31 +0200

    Also consider the state of Validator.types_mapping in context of valid schemas

--- cerberus/tests/test_schema.py
+++ cerberus/tests/test_schema.py
@@ -83,7 +83,7 @@ def test_validated_schema_cache():
     v = Validator({'foozifix': {'coerce': int}})
     assert len(v._valid_schemas) == cache_size
 
-    max_cache_size = 133
+    max_cache_size = 139
     assert cache_size <= max_cache_size, \
         "There's an unexpected high amount (%s) of cached valid " \
         "definition schemas. Unless you added further tests, " \
