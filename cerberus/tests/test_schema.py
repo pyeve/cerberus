73a8d4da302874d77d1f41d109347142b1fb19ca
Frank Sachsenheim <funkyfuture@riseup.net>
2017-10-04 00:04:56 +0200

    Adds tests for nested normalizations

--- cerberus/tests/test_schema.py
+++ cerberus/tests/test_schema.py
@@ -88,7 +88,7 @@ def test_validated_schema_cache():
     v = Validator({'foozifix': {'coerce': int}})
     assert len(v._valid_schemas) == cache_size
 
-    max_cache_size = 141
+    max_cache_size = 143
     assert cache_size <= max_cache_size, \
         "There's an unexpected high amount (%s) of cached valid " \
         "definition schemas. Unless you added further tests, " \
