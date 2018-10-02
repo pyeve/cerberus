2350a58fff07adf60baf240fee4b09367afebcc4
Frank Sachsenheim <funkyfuture@riseup.net>
2018-10-03 00:43:34 +0200

    Adds type annotations to the utils module

--- cerberus/utils.py
+++ cerberus/utils.py
@@ -1,71 +1,37 @@
-from collections import Mapping, namedtuple, Sequence, Set
-
-TypeDefinition = namedtuple('TypeDefinition', 'name,included_types,excluded_types')
-"""
-This class is used to define types that can be used as value in the
-:attr:`~cerberus.Validator.types_mapping` property.
-The ``name`` should be descriptive and match the key it is going to be assigned
-to.
-A value that is validated against such definition must be an instance of any of
-the types contained in ``included_types`` and must not match any of the types
-contained in ``excluded_types``.
-"""
+from typing import Tuple
 
 
 def compare_paths_lt(x, y):
-    for i in range(min(len(x), len(y))):
-        if isinstance(x[i], type(y[i])):
-            if x[i] != y[i]:
-                return x[i] < y[i]
-        elif isinstance(x[i], int):
+    min_length = min(len(x), len(y))
+
+    if x[:min_length] == y[:min_length]:
+        return len(x) == min_length
+
+    for i in range(min_length):
+        a, b = x[i], y[i]
+
+        for _type in (int, str, tuple):
+            if isinstance(a, _type):
+                if isinstance(b, _type):
+                    break
+                else:
+                    return True
+
+        if a == b:
+            continue
+        elif a < b:
             return True
-        elif isinstance(y[i], int):
+        else:
             return False
 
     raise RuntimeError
 
 
-def drop_item_from_tuple(t, i):
+def drop_item_from_tuple(t: Tuple, i: int) -> Tuple:
     return t[:i] + t[i + 1 :]
 
 
-def get_Validator_class():
-    global Validator
-    if 'Validator' not in globals():
-        from cerberus.validator import Validator
-    return Validator
-
-
-def mapping_hash(schema):
-    return hash(mapping_to_frozenset(schema))
-
-
-def mapping_to_frozenset(mapping):
-    """ Be aware that this treats any sequence type with the equal members as
-        equal. As it is used to identify equality of schemas, this can be
-        considered okay as definitions are semantically equal regardless the
-        container type. """
-
-    aggregation = {}
-
-    for key, value in mapping.items():
-        if isinstance(value, Mapping):
-            aggregation[key] = mapping_to_frozenset(value)
-        elif isinstance(value, Sequence):
-            value = list(value)
-            for i, item in enumerate(value):
-                if isinstance(item, Mapping):
-                    value[i] = mapping_to_frozenset(item)
-            aggregation[key] = tuple(value)
-        elif isinstance(value, Set):
-            aggregation[key] = frozenset(value)
-        else:
-            aggregation[key] = value
-
-    return frozenset(aggregation.items())
-
-
-def quote_string(value):
+def quote_string(value: str) -> str:
     if isinstance(value, str):
         return '"%s"' % value
     else:
@@ -73,40 +39,11 @@ def quote_string(value):
 
 
 class readonly_classproperty(property):
-    def __get__(self, instance, owner):
+    def __get__(self, instance, owner):  # type: ignore
         return super().__get__(owner)
 
-    def __set__(self, instance, value):
+    def __set__(self, instance, value):  # type: ignore
         raise RuntimeError('This is a readonly class property.')
 
-    def __delete__(self, instance):
+    def __delete__(self, instance):  # type: ignore
         raise RuntimeError('This is a readonly class property.')
-
-
-def validator_factory(name, bases=None, namespace={}):
-    """ Dynamically create a :class:`~cerberus.Validator` subclass.
-        Docstrings of mixin-classes will be added to the resulting
-        class' one if ``__doc__`` is not in :obj:`namespace`.
-
-    :param name: The name of the new class.
-    :type name: :class:`str`
-    :param bases: Class(es) with additional and overriding attributes.
-    :type bases: :class:`tuple` of or a single :term:`class`
-    :param namespace: Attributes for the new class.
-    :type namespace: :class:`dict`
-    :return: The created class.
-    """
-    Validator = get_Validator_class()
-
-    if bases is None:
-        bases = (Validator,)
-    elif isinstance(bases, tuple):
-        bases += (Validator,)
-    else:
-        bases = (bases, Validator)
-
-    docstrings = [x.__doc__ for x in bases if x.__doc__]
-    if len(docstrings) > 1 and '__doc__' not in namespace:
-        namespace.update({'__doc__': '\n'.join(docstrings)})
-
-    return type(name, bases, namespace)
