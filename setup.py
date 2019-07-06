3b3f28082a35ac251a66b1539be9151364139598
Frank Sachsenheim <funkyfuture@riseup.net>
2019-07-06 15:46:00 +0200

    Bumps version to 1.3.2dev0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = (
     "Python dictionaries."
 )
 LONG_DESCRIPTION = open("README.rst").read()
-VERSION = "1.3.1"
+VERSION = "1.3.2dev0"
 
 setup_requires = (
     ["pytest-runner"] if any(x in sys.argv for x in ("pytest", "test", "ptr")) else []
