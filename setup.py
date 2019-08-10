2f34b90147cd9334c812f4ce39e24cac57593b2b
Frank Sachsenheim <funkyfuture@riseup.net>
2019-08-10 17:00:26 +0200

    Adds setuptools as dependency
    Fixes #499

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     setup_requires=setup_requires,
     tests_require=["pytest"],
     test_suite="cerberus.tests",
-    install_requires=[],
+    install_requires=["setuptools"],
     keywords=["validation", "schema", "dictionaries", "documents", "normalization"],
     python_requires=">=3.5",
     classifiers=[
